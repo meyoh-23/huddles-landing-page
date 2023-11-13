29340af4116bb0379cd783df61e9cf748130a88b
Felix <moxiemeyoh@gmail.com>
2023-11-13 14:33:39 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <p className='mx-10 bg-secondary'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eligendi laborum porro obcaecati tempora, quaerat aliquam ea sequi totam ut dignissimos et consequuntur, omnis libero eum voluptatem asperiores in at.
+      </p>
+    </>
+  )
+}
+
+export default App
