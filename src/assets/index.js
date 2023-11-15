2fa01f40edd67dbf0afee231c16a7b596ae2584a
Felix <moxiemeyoh@gmail.com>
2023-11-15 12:42:50 +0300

    stats completed

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,15 @@
 import desktopFooterTopPapptern from "./bg-footer-top-desktop.svg";
 import mobileFooterTopPattern from "./bg-footer-top-mobile.svg";
 import logo from "./logo.svg";
+import screenMockup from "./screen-mockups.svg";
+import messageIcon from "./icon-messages.svg";
+import communitiesIcon from "./icon-communities.svg";
 
 export {
     mobileFooterTopPattern, 
     desktopFooterTopPapptern,
     logo,
+    screenMockup,
+    messageIcon,
+    communitiesIcon,
 };
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { communitiesIcon, messageIcon, screenMockup } from '../assets';
 
 const Stats = () => {
   return (
-    <div>stats</div>
+    <div className='w-full lg:max-w-[1400px] flex flex-col my-6 md:my-16'>
+      <div className='w-full lg:max-w-[1400px] px-6 md:px-16'>
+        <img src={screenMockup} alt="mockups"  className='mx-auto'/>
+      </div>
+      <div className='w-full lg:max-w-[1400px] px-6 md:px-16 flex flex-col sm:flex-row items-center justify-between gap-y-6 sm:gap-y-16'>
+        <div className='flex flex-col items-center sm:items-start'>
+          <img src={communitiesIcon} alt="community" />
+          <h1>1.4K+</h1>
+          <p className='stats__message'>Communities Formed</p>
+        </div>
+        <div className='flex flex-col items-center sm:items-start'>
+          <img src={messageIcon} alt="messages" />
+          <h1>2.7m+</h1>
+          <p className='stats__message'>Messages sent</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
